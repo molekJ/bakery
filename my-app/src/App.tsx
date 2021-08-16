5e3c99aaa7c6eb763ebe2a9fa9ff3a03107dcd08
Jakub Molicki <molickikuba@gmail.com>
2021-08-16 13:32:23 +0200

    Display flourAmount added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,17 +5,22 @@ import { DisplayAmount } from './Components/DisplayAmount';
 function App() {
 
 
+  const [flourAmount, setFlourAmount] = useState<number>(100);
   const [progresDought, setProgresDought] = useState<number>(0);
   const [isMakingDought, setisMakingDought] = useState<boolean>(false);
   const [doughtAmount, setDoughtAmount] = useState<number>(0);
 
 
   useEffect(() => {
+    if(flourAmount < 10){
+      setisMakingDought(false);
+      return;
+    }
     if(isMakingDought){
-
-      if(progresDought === 100){
+       if(progresDought === 100){
        setProgresDought(0);
        setDoughtAmount(doughtAmount + 1);
+       setFlourAmount(flourAmount - 10);
        return;
       }
       setTimeout(()=>{
@@ -25,9 +30,10 @@ function App() {
       
       return (
         <div className="App">
-      <h1>BACERY</h1>
-      <ProgressBar progressValue={progresDought}/>
-      <button onClick={() => {
+           <h1>BACERY</h1>
+           <ProgressBar progressValue={progresDought}/>
+           <DisplayAmount amount={flourAmount} nameAmount={"Ilość dostępnej mąki:"}/>
+      <button disabled={flourAmount<10? true : false} onClick={() => {
         setisMakingDought(!isMakingDought)
       }}>{isMakingDought ?'Zatrzymaj lepienie' : 'Ulep ciasto'}</button>
       <DisplayAmount amount={doughtAmount} nameAmount={"liczba ulepionych kul"}/>
---
+++ my-app/src/Components/DoughShelf.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export const DoughShelf:React.FC<{ number: number}> = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
